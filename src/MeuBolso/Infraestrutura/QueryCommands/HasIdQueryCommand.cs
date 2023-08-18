38f902ad97dfd2b63046ced66e1844fa5e1528dd
João Pedro Monteiro <joao.joaopedro@hotmail.com>
2023-08-18 18:03:34 -0300

    implementando a classe HasIdQueryCommand

---
+++ src/MeuBolso/Infraestrutura/QueryCommands/HasIdQueryCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace MeuBolso.Infraestrutura.QueryCommands
+{
+    public class HasIdQueryCommand <T> : PaginationQueryCommand<T>
+    {
+        public Guid? Id { get; set; }
+        public HashSet<Guid>? Ids { get; set; }
+        public HashSet<Guid>? IgnoreIds { get; set; }
+       
+
+        public override IQueryable<T> Apply(IQueryable<T> queryable)
+        {
+            base.Apply(queryable);
+
+            if (Id.HasValue)
+                queryable = queryable.Where(Id == Id.Value);
+
+            if (Ids != null && Ids.Any())
+                queryable = queryable.Where(Ids.Contains(Id));
+
+            if (IgnoreIds != null && IgnoreIds.Any())
+                queryable = queryable.Where(IgnoreIds.Contains(Id));
+
+            return queryable;
+        }
+    }
+}
--- src/MeuBolso/Modulos/Carteira/QueryCommands/CarteiraQueryCommand.cs
+++ src/MeuBolso/Modulos/Carteira/QueryCommands/CarteiraQueryCommand.cs
@@ -3,24 +3,11 @@ using MeuBolso.Modulos.Carteira.Entidades;
 
 namespace MeuBolso.Modulos.Carteira.QueryCommands;
 
-public class CarteiraQueryCommand : PaginationQueryCommand<CarteiraEntity>
+public class CarteiraQueryCommand : HasIdQueryCommand<CarteiraEntity>
 {
-    public Guid? Id { get; set; }
-    public HashSet<Guid>? Ids { get; set; }
-    public HashSet<Guid>? IgnoreIds { get; set; }
     public string? Query { get; set; }
-
     public override IQueryable<CarteiraEntity> Apply(IQueryable<CarteiraEntity> queryable)
     {
-        if (Id.HasValue)
-            queryable = queryable.Where(w => w.Id == Id.Value);
-
-        if (Ids != null && Ids.Any())
-            queryable = queryable.Where(w => Ids.Contains(w.Id));
-
-        if (IgnoreIds != null && IgnoreIds.Any())
-            queryable = queryable.Where(w => !IgnoreIds.Contains(w.Id));
-
         if (!string.IsNullOrEmpty(Query))
             queryable = queryable.Where(w => w.Descricao!.Contains(Query));
 
