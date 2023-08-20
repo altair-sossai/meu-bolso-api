d0ac9310b99fd66d8e7f43865b62baef9e7f75b9
Altair Sossai <altairsossai@gmail.com>
2023-08-20 11:06:43 -0300

    WIP

--- src/MeuBolso/Modulos/Carteira/Profiles/CarteiraProfile.cs
+++ src/MeuBolso/Modulos/Carteira/Profiles/CarteiraProfile.cs
@@ -4,13 +4,10 @@ using MeuBolso.Modulos.Carteira.Entidades;
 
 namespace MeuBolso.Modulos.Carteira.Profiles;
 
-// Classe responsável por definir os mapeamentos entre tipos relacionados à entidade Carteira.
 public class CarteiraProfile : Profile
 {
     public CarteiraProfile()
     {
-        // Cria um mapeamento entre a classe CarteiraCommand e a classe CarteiraEntity.
         CreateMap<CarteiraCommand, CarteiraEntity>();
-        // Esse mapeamento permite que os valores de CarteiraCommand sejam copiados para uma instância de CarteiraEntity.
     }
 }
